93b1a7784cfa15199c2b200fa1b0c9b9ac25a24f
Paul Hudson <phudson@pobox.com>
2025-10-07 20:20:58 +0100

    Missed one

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ from typing import Any, Dict
 
 import pytest
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
 
 from runpod_llm_manager.config import AppConfig
 from runpod_llm_manager.dependencies import Dependencies, create_test_dependencies
@@ -29,13 +28,6 @@ def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
-
-
-@pytest.fixture
-async def async_client():
-    """Async client for testing FastAPI app."""
-    async with AsyncClient(app=app, base_url="http://testserver") as client:
-        yield client
 
 
 @pytest.fixture
